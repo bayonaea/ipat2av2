b2606de6837d0c8d2adf35bf7afe19eb8a67edb1
bayonaea <bayona.ea.bsinfotech@gmail.com>
2024-05-01 19:30:36 +0800

    Second Commit

--- ipat2A/dcrm/models.py
+++ ipat2A/dcrm/models.py
@@ -12,5 +12,7 @@ class Record(models.Model):
     city =  models.CharField(max_length=50)
     state =  models.CharField(max_length=50)
     zipcode =  models.CharField(max_length=20)
+
+
     def __str__(self):
         return(f"{self.first_name} {self.last_name} {self.email}")
--- ipat2A/dcrm/views.py
+++ ipat2A/dcrm/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .models import Record
 
+from .forms import SignUpForm
 # Create your views here.
 
 
 def home(request):
-    #DO SOMETHING
+    #do something
+    records = Record.objects.all()
 
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'records':records})
+
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+                form.save()
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password1']
+                user = authenticate(username=username, password=password)
+                login(request, user)
+                messages.success(request, "You Have Successfully Registered! WELCOME!")
+                return redirect('home')
+        
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
